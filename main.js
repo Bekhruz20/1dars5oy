8508daf9ce878b7fb86b8d2ee6e35f739ebc4256
Bekhruz20 <163981178+Bekhruz20@users.noreply.github.com>
2024-06-27 13:14:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+let data = +promt("Hafta kunini son bilan kiriting")
+
+switch (data) {
+    case 1:
+      day = "Dushanba";
+      break;
+    case 2:
+      day = "Seshanba";
+      break;
+    case 3:
+       day = "Chorshanba";
+      break;
+    case 4:
+      day = "Payshanba";
+      break;
+    case 5:
+      day = "Juma";
+      break;
+    case 6:
+      day = "Shamba";
+      break;
+    case 7:
+      day = "Yakshanba";
+        default:
+    console.log ("Hafta kunini kiritishingizni sorab qolamiz")
+    break;
+  }
+
+
+// let n = +promt("Oy sonini kiriting")
+
+// switch (n) {
+//   case 1:
+//     console.log("yanvar")
+//     break;
+//     case 2:
+//       console.log("fevral")
+//       break;
+//       case 3:
+//     console.log("mart")
+//     break;
+//     case 4:
+//     console.log("aprel")
+//     break;
+//     case 5:
+//     console.log("may")
+//     break;
+//     case 6:
+//     console.log("iyun")
+//     break;
+//     case 7:
+//     console.log("iyul")
+//     break;
+//     case 8:
+//     console.log("avgust")
+//     break;
+//     case 9:
+//     console.log("sentabr")
+//     break;
+//     case 10:
+//     console.log("oktabr")
+//     break;
+//     case 11:
+//     console.log("noyabr")
+//     break;
+//     case 12:
+//     console.log("dekabr")
+//     break;
+//   default:
+//   console.log ("Oy kiritishingizni sorab qolamiz")
+//     break;
+// }
